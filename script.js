9e4839ce91ec8785dede54507dd4412803d4c393
SofiaBrittoSchwartz <sofia.b.schwartz@gmail.com>
2024-11-05 14:42:00 -0800

    Added Delivery Partner Actions div and functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+function getActionsDiv() {
+    let actionsDiv = document.querySelector(".actions");
+
+    /* If actionsDiv already exists, reset it */
+    if(actionsDiv !== null) {
+        actionsDiv.innerHTML = "";
+    } else {
+        actionsDiv = document.createElement('div');
+        actionsDiv.className = "actions";
+    }
+
+    return actionsDiv;
+}
+
 function renderRestaurantActions() {
     const container = document.querySelector('.containers');
-    
+
     /* Outer div */
-    const actionsDiv = document.createElement('div');
-    actionsDiv.className = "actions";
+    let actionsDiv = getActionsDiv();
     
     /* Title */
     const title = document.createElement('h2');
@@ -34,6 +47,44 @@ function renderRestaurantActions() {
     container.appendChild(actionsDiv);
 }
 
+function renderDeliveryPartnerActions() {
+    const container = document.querySelector('.containers');
+    
+    /* Outer div */
+    const actionsDiv = getActionsDiv();
+    
+    /* Title */
+    const title = document.createElement('h2');
+    title.innerHTML = 'Delivery Partner Actions';
+
+    /* Buttons */
+    const buttons = document.createElement('div');
+    buttons.className = "buttons";
+    buttons.id = "delivery-btns"
+
+    const assignButton = document.createElement('button');
+    assignButton.id = "assign-btn";
+    assignButton.innerHTML = "Assign Delivery Partner";
+
+    buttons.appendChild(assignButton);
+
+    /* Informational text */
+    const infoText = document.createElement('span');
+    infoText.id = "delivery-info-text";
+    infoText.innerText = "Delivery partner is being assigned...";
+
+    actionsDiv.appendChild(title);
+    actionsDiv.appendChild(buttons);
+    actionsDiv.appendChild(infoText);
+
+    container.appendChild(actionsDiv);
+}
+
+function updateOrderStatus(status) {
+    const orderStatus = document.getElementById("orderStatus");
+    orderStatus.innerHTML = status;
+}
+
 async function placeOrder() {
     
     renderRestaurantActions();
@@ -41,26 +92,66 @@ async function placeOrder() {
     const orderStatus = document.getElementById('orderStatus');
     orderStatus.innerHTML = "Order placed! Waiting for restaurant response...";
 
-    console.log("Place order");
+    console.info("Placing order...");
     try {
         const orderResponse = await new Promise((resolve, reject) => {
             const acceptButton = document.getElementById('accept-btn');
             const declineButton = document.getElementById('decline-btn');
             
-            acceptButton.addEventListener('click', () => resolve('Approved'));
-            declineButton.addEventListener('click', () => reject('Declined'));
+            acceptButton.addEventListener('click', () => resolve('Order approved'));
+            declineButton.addEventListener('click', () => reject('Order declined'));
+
             setTimeout(() => reject("Time ran out"), 60000);
         });
 
-        console.log(orderResponse);
+        updateOrderStatus("Order accepted by restaurant. Waiting for delivery partner...");
+        
+        console.info(orderResponse);
+
+        const actionsDiv = document.querySelector('.actions');
+
+        renderDeliveryPartnerActions();
+
+        const deliveryResponse = await new Promise((resolve, reject) => {
+            const assignButton = document.getElementById('assign-btn');
+            assignButton.addEventListener('click', () => resolve("Delivery partner assigned"));
+            setTimeout(() => reject("Time ran out"), 60000);
+        });
+
+        console.info(deliveryResponse);
+
+        updateOrderStatus("Delivery partner assigned. Waiting for delivery...")
+
+        /* Update Delivery Partner Actions Div */
+        const buttonsDiv = document.getElementById('delivery-btns');
+        
+        const outForDeliveryBtn = document.createElement('button');
+        outForDeliveryBtn.id = "out-for-delivery-btn";
+        outForDeliveryBtn.innerHTML = "Out for Delivery";
+        
+        buttonsDiv.appendChild(outForDeliveryBtn);
+
+        const infoText = document.getElementById("delivery-info-text");
+        infoText.innerHTML = "Delivery partner assigned!";
+
+        const outForDeliveryResponse = await new Promise((resolve, reject) => {
+            outForDeliveryBtn.addEventListener('click', () => resolve("Order out for delivery"));
+            setTimeout(() => reject("Time ran out"), 60000);
+        });
+
+        console.info(outForDeliveryResponse);
+
+        infoText.innerHTML = "Order is out for delivery!";
+        updateOrderStatus("Order is out for delivery! It will arrive soon.");
+
     } catch (error) {
         const actionsDiv = document.querySelector('.actions');
         actionsDiv.remove();
 
         const orderStatusDiv = document.getElementById('orderStatus');
-        orderStatusDiv.innerHTML = "Order declined. Please try again!";
+        orderStatusDiv.innerHTML = "Order cancelled. Please try again!";
 
-        console.log(error);
+        console.warn(error);
     }
 }
 
